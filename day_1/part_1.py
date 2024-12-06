2c5d799a423f7476fb51480168013e265abe140e
Gabriel Romero <gabriromero@MacBook-Pro-de-Gabriel.local>
2024-12-06 21:02:25 +0100

    optimized day 1

--- day_1/part_1.py
+++ day_1/part_1.py
@@ -1,33 +1,23 @@
 from typing import List, Tuple
 
-def insert_in_ordered_list(list: List[int], number: int):
-    low = 0
-    high = len(list)
-
-    while low != high:
-        mid = (low+high) // 2
-        if list[mid] < number:
-            low = mid + 1
-        else:
-            high = mid
-
-    list.insert(low, number)
-
 def retrieve_numbers_from_input() -> Tuple[List[int], List[int]]:
     left_numbers: List[int] = []
     right_numbers: List[int] = []
 
-    with open('day_1/inputs/input_part_1.txt') as file:
+    with open('day_1/data/input.txt') as file:
         for line in file.readlines():
             line_numbers: List[str] = line.split()
-            insert_in_ordered_list(left_numbers, int(line_numbers[0]))
-            insert_in_ordered_list(right_numbers, int(line_numbers[1]))
+            left_numbers.append(int(line_numbers[0]))
+            right_numbers.append(int(line_numbers[1]))
 
     return (left_numbers, right_numbers)
 
 
 def main():
     left_numbers, right_numbers = retrieve_numbers_from_input()
+    
+    left_numbers.sort()
+    right_numbers.sort()
     
     sum = 0
     for i in range(len(left_numbers)):
